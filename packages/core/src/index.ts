8e17211f7bbbc8f105c266e085fceb8f4b01d33f
ityuany <519495771@qq.com>
2023-03-19 16:28:09 +0800

    'feat: Add commit message generator based on diff information
    This commit adds a new feature to generate commit messages based on git diff information, using either GPT-3.5-turbo or GPT-4 language models, depending on the `mode` setting specified by the user in the `UserConfig` object. The commit message is generated in either English or Chinese, depending on the `lang` setting. The commit message is returned by calling the `createCommitMessageCreator` function with a `UserConfig` object that includes the API key, mode, and language settings.
    
    The commit message generator uses the `prompt` object to provide a guide message for the user. The guide message is in either English or Chinese, depending on the `lang` setting. The generated commit messages are based on the diff information passed to the `createChatCompletion` function, and are structured in accordance with the standard git message format. The `description` section is organized in list form to provide a clear, detailed summary of the changes made.'

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,9 +2,24 @@ import { OpenAIApi, Configuration, ChatCompletionRequestMessage } from "openai";
 
 export interface UserConfig {
   apiKey: string;
-  mode: "gpt-3.5-turbo";
-  prompt: string;
+  mode: "gpt-3.5-turbo" | "gpt-4";
+  lang: "cn" | "en";
 }
+
+export const prompt = {
+  cn: `
+    现在开始，假装你是一个高级程序员。
+    你精通 git ，你接下来的主要工作就是根据用户传输给你的 git diff 信息 使用中文推荐给用户一段 commit message！
+    内容尽可能的详细，以 标准的 git message 格式给用户。区分清楚 subject 和 description , description 以列表形式组织。
+    请直接返回 commit message 信息，不要说多余的话
+    `,
+  en: `
+    Starting now, pretend that you are a senior programmer.
+    You are proficient in git. Your main task is to use the English language to recommend a commit message to users based on the git diff information transmitted by them!
+    The content should be as detailed as possible and presented in standard git message format. Clearly distinguish between subject and description, with the description organized in list form.
+    Please return only the commit message information directly without any extra words.
+    `,
+};
 
 export function createCommitMessageCreator(config: UserConfig) {
   const configuration = new Configuration({
@@ -16,13 +31,14 @@ export function createCommitMessageCreator(config: UserConfig) {
     const GPT35TurboMessage: ChatCompletionRequestMessage[] = [
       {
         role: "system",
-        content: config.prompt,
+        content: prompt[config.lang],
       },
       {
         role: "user",
         content: diffMessage,
       },
     ];
+
     const completion = await openai.createChatCompletion({
       model: config.mode,
       messages: GPT35TurboMessage,
